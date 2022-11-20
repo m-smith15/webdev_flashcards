c2c51e91d3a60b4c98dcee778d5b5bab72d9c217
m-smith15 <mpsmith1215@gmail.com>
2022-11-20 14:24:36 -0500

    front end updates

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -19,7 +19,7 @@ const CardList = (props) => {
                     <div className="title">| Title | <br/> {card.cardTitle}</div> //false (starts false by default)
                     }
                     <br />
-                    <button onClick={() => { editCard(card._id)}} className="btn btn-secondary">
+                    <button onClick={() => { editCard(card._id)}} className="btn btn-sm btn-secondary">
                         Edit
                     </button>
                 </div>
