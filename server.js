a60ae68a2d6396c64240b0fe268763eeef1e709c
m-smith15 <mpsmith1215@gmail.com>
2022-11-22 00:50:19 -0500

    config changes

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const port = 8000;
 // require('./server/config/mongoose.config');
 
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    app.listen(port, () => console.log(`Spinning up on port: ${port}`) );
+})
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true } ) ); 
 require('./server/routes/card.routes')(app);
     
-app.listen(port, () => console.log(`Spinning up on port: ${port}`) );
