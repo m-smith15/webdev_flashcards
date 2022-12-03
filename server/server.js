3726072538203e13ee1d91256b3fceac00178014
m-smith15 <mpsmith1215@gmail.com>
2022-12-03 18:23:11 -0500

    restructuring

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 8000;
 require('dotenv').config()
 //requiring the techs - express, cors
-require('./server/config/mongoose.config');
+require('./config/mongoose.config');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true } ) );
-require('./server/routes/card.routes')(app);
+require('./routes/card.routes')(app);
 app.listen(port, () => console.log(`Spinning up on port: ${port}`) );
 
