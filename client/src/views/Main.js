a8b65a54c02569be4082c482ae171786288a7dfd
m-smith15 <mpsmith1215@gmail.com>
2022-12-04 02:56:35 -0500

    more styling updates

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -13,9 +13,10 @@ const Main = (props) => {
         <div>
             <h1>WebDev Flashcards</h1>
             <p>Welcome to webdev flashcards! <br />
-                The purpose of this app is to help aspiring webdevs practice and study up on concepts within the industry</p>
+                The purpose of this app is to help aspiring webdevs practice and study up on concepts within the industry. <br />
+                Peep the repo <a href="https://github.com/m-smith15/webdev_flashcards">here</a></p>
             <hr />
-            <div>Ready to get practicing?</div>
+            <p>Ready to get practicing?</p>
             <button onClick={navigateToPractice}>
                 Lets get moving!</button>
         </div>
