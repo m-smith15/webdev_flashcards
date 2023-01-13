47f381198db3cb1f39e06ce65376f7e7e270f672
m-smith15 <mpsmith1215@gmail.com>
2023-01-13 16:43:47 -0500

    moving link to repo to footer

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -14,7 +14,7 @@ const Main = (props) => {
             <h1>WebDev Flashcards</h1>
             <p>Welcome to webdev flashcards! <br />
                 The purpose of this app is to help aspiring webdevs practice and study up on concepts within the industry. <br />
-                Peep the repo <a href="https://github.com/m-smith15/webdev_flashcards">here</a></p>
+            </p>
             <hr />
             <p>Ready to get practicing?</p>
             <button onClick={navigateToPractice}>
