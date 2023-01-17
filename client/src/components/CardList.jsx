9d97b23a3cf5bb0dc4be2f6c7a9a544817ae1266
m-smith15 <mpsmith1215@gmail.com>
2023-01-17 17:06:59 -0500

    update cardlist 2 reset showdesc on set chng

--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const CardList = (props) => {
     const navigate = useNavigate();
     const [showDescription, setShowDescription] = useState(null);
+    //passing sets in from Practice view - going to use this to reset the "showDescription" to false
+
+    useEffect(() => {
+            if(props.sets > 0) {
+                setShowDescription(null);
+                console.log("ran");
+                console.log(props.card);
+            }
+    }, [props.sets])
 
     const editCard = (cardId) => {
         navigate('/edit/' + cardId)
@@ -14,16 +23,17 @@ const CardList = (props) => {
             {props.card.map((card, i) =>
                 <div className="card" key={i} onClick={() => { setShowDescription(i) }}>
                     {showDescription === i ?
-                    <div className="description"><u>Description</u><br/> {card.cardDescription}</div> //true
+                        <div className="description"><u>Description</u><br /> {card.cardDescription}</div> //true
                         :
-                    <div className="title"><u>Title</u><br/> {card.cardTitle}</div> //false (starts false by default)
+                        <div className="title"><u>Title</u><br /> {card.cardTitle}</div> //false (starts false by default)
                     }
                     <br />
-                    <button onClick={() => { editCard(card._id)}} className="btn btn-sm btn-secondary">
+                    <button onClick={() => { editCard(card._id) }} className="btn btn-sm btn-secondary">
                         Edit
                     </button>
                 </div>
             )}
+            <div>{props.sets}</div>
         </div>
     )
 }
