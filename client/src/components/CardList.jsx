83caf118202477ba7fd3fc0c34098901965bdba4
unclebabykern <coren.frankel@gmail.com>
2023-02-10 17:37:50 -0700

    Working flashcard flip; needs style fixes

--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -1,48 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ReactCardFlip from 'react-card-flip';
 
-const CardList = (props) => {
+const Card = ({card}) => {
     const navigate = useNavigate();
-    const [showDescription, setShowDescription] = useState(null);
+    const [isFlipped, setIsFlipped] = useState(false);
     //passing sets in from Practice view - going to use this to reset the "showDescription" to false
-
-    //this will reset setShowDescription to null when the set counter increases
-    useEffect(() => {
-            if(props.sets > 0) {
-                setShowDescription(null);
-            }
-    }, [props.sets])
 
     const editCard = (cardId) => {
         navigate('/edit/' + cardId)
     }
 
     return (
+        <ReactCardFlip className="card" isFlipped={isFlipped} flipDirection='horizontal'>
+            <div className="title CardFront" onClick={()=>setIsFlipped((prev) => !prev)}>
+                {card.cardTitle}
+                <button onClick={() => { editCard(card._id) }} className="btn btn-sm btn-secondary">
+                    Edit
+                </button>
+            </div>
+            <div className="description CardBack" onClick={()=>setIsFlipped((prev) => !prev)}>
+                <h5>{card.cardTitle}</h5><br/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <hr class="blueline"/>
+                <p>{card.cardDescription}</p>
+            </div>
+        </ReactCardFlip>
+    )
+}
+
+const CardList = (props) => {
+    return (
         <div className="cardContainer container">
             {props.card.map((card, i) =>
-                <div className="card" key={i} onClick={() => { setShowDescription(i) }}>
-                    {showDescription === i ?
-                        <div className="description">
-                            <h5>{card.cardTitle}</h5><br/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <hr class="blueline"/>
-                            <p>{card.cardDescription}</p>
-                        </div> //true
-                        :
-                        <div className="title">{card.cardTitle}</div> //false (starts false by default)
-                    }
-                    <br />
-                    <button onClick={() => { editCard(card._id) }} className="btn btn-sm btn-secondary">
-                        Edit
-                    </button>
-                </div>
+                <Card card={card} key={`card-${i}`}/>
             )}
         </div>
     )
