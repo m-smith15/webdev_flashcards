86e72ac7919dbd2adf106fab0a6792f73dfc7d62
unclebabykern <coren.frankel@gmail.com>
2023-02-13 15:28:29 -0700

    Add validations logic and amend jsx requirements
    With the errors list object set in state, the catch for the POST request now accrues a list of errors, then passes them into the form component to show the user before the form can submit. Last change to <path> element is html-jsx property translation.

--- client/src/views/Create.js
+++ client/src/views/Create.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 const Create = () => {
     const navigate = useNavigate();
+    const [errors, setErrors] = useState([]);
     //Conditionally render the api endpoint base url
     const BASE_URL = process.env.NODE_ENV === 'production'
         // for production 
@@ -14,11 +15,19 @@ const Create = () => {
         : 'http://localhost:8000';
     const createCard = card => {
         axios.post(BASE_URL + '/api/create/card', card)
-            .then(res =>
-                console.log(res),
-                // go back to view all
-                navigate('/viewall'))
-            .catch(err => console.log(err))
+            .then(res => {
+                console.log(res)
+                navigate('/viewall')
+                })
+                // on success go back to view all
+            .catch(err => {
+                // Unpack errors into iterable array
+                const errorResponse = err.response.data.errors;
+                const errorArr = [];
+                for (const key of Object.keys(errorResponse)) {
+                    errorArr.push(errorResponse[key].message)
+                }
+                setErrors(errorArr);})
     }
 
     return (
@@ -27,13 +36,14 @@ const Create = () => {
             <br />
             <CardForm
                 onSubmitProp={createCard}
-                initialTitle={''}
-                initiaDesc={''}
-                inputVal={'Add Card'} />
+                errors={errors}
+                initialTitle=''
+                initiaDesc=''
+                inputVal='Add Card' />
             <hr />
             <button className='btn btn-dark' onClick={() => navigate(-1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle me-2" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
                 </svg>
                 Go Back
             </button>
