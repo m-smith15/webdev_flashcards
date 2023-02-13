a3d6a9ad4d5acc74311f9193d5045d6a171065ac
unclebabykern <coren.frankel@gmail.com>
2023-02-13 15:30:53 -0700

    Update Edit.js w/ validation calibration
    With the errors list object set in state, the catch for the POST request now accrues a list of errors, then passes them into the form component to show the user before the form can submit. Last change to <path> element is html-jsx property translation.
    TL;DR: same as the Create.js changes in previous commit

--- client/src/views/Edit.js
+++ client/src/views/Edit.js
@@ -5,14 +5,15 @@ import { useParams, useNavigate } from 'react-router-dom';
 
 const Update = () => {
     const { id } = useParams();
+    const [card, setCard] = useState();
+    const [loaded, setLoaded] = useState(false);
+    const [errors, setErrors] = useState([]);
     //Conditionally render the api endpoint base url 
     const BASE_URL = process.env.NODE_ENV === 'production'
         // for production 
         ? process.env.REACT_APP_SERVER_URL
         // or development environment
         : 'http://localhost:8000'
-    const [card, setCard] = useState();
-    const [loaded, setLoaded] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,11 +26,16 @@ const Update = () => {
 
     const updateCard = card => {
         axios.put(BASE_URL + '/api/card/' + id, card)
-            .then(res =>
-                console.log(res),
+            .then(res => navigate('/viewall'))
                 // after editing go to viewall
-                navigate('/viewall'))
-            .catch(err => console.error(err));
+            .catch(err => {
+                // Unpack errors into iterable array
+                const errorResponse = err.response.data.errors;
+                const errorArr = [];
+                for (const key of Object.keys(errorResponse)) {
+                    errorArr.push(errorResponse[key].message)
+                }
+                setErrors(errorArr);});
     }
 
     const deleteCard = (cardId) => {
@@ -48,13 +54,14 @@ const Update = () => {
                     <CardForm
                         // pass update fn and text to form
                         onSubmitProp={updateCard}
+                        errors={errors}
                         initialTitle={card.cardTitle}
                         initialDesc={card.cardDescription}
-                        inputVal={'Update'}
+                        inputVal='Update'
                     />
                     <button className='btn btn-dark me-2' onClick={() => navigate(-1)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left-circle me-2" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+                            <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
                         </svg>
                         Go Back
                     </button>
