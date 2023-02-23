70105d32cfd1d43a3531c93ba7ea6483eb2cf8de
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-02-23 03:50:43 -0500

    adding toggle function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const navigate = useNavigate();
+    const [setTheme, theme] = useState(false);
 
     const navigateToPractice = () => {
         navigate('/practice')
@@ -13,6 +14,11 @@ const Header = () => {
     const navigateToViewAll = () => {
         navigate('/viewall')
     }
+    const toggleThemeChange = (theme) => {
+        console.log(theme);
+        setTheme(!theme);
+        console.log(theme);
+    }
 
     return (
         <div className="header">
@@ -21,12 +27,12 @@ const Header = () => {
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToHome}><h2>Home</h2></button></div>
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToPractice}><h2>Practice</h2></button></div>
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToViewAll}><h2>View All</h2></button></div>
-                <div className="navigationItem">
+                <div className="navigationItem lightdarkToggle">
                     <h2>
-                    <label class="switch">
-                        <input type="checkbox" />
-                        <span class="slider"/>
-                    </label>
+                        <label class="switch">
+                            <input type="checkbox" onClick={toggleThemeChange}/>
+                            <span class="slider" />
+                        </label>
                     </h2>
                     </div>
             </div>
