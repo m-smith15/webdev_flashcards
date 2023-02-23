99a5b4c3795d880ef8842ec462cd5e183e02ddd7
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-02-23 03:26:29 -0500

    adding in toggle

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,6 +21,14 @@ const Header = () => {
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToHome}><h2>Home</h2></button></div>
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToPractice}><h2>Practice</h2></button></div>
                 <div className="navigationItem"><button type="button" className="btn btn-dark" onClick={navigateToViewAll}><h2>View All</h2></button></div>
+                <div className="navigationItem">
+                    <h2>
+                    <label class="switch">
+                        <input type="checkbox" />
+                        <span class="slider"/>
+                    </label>
+                    </h2>
+                    </div>
             </div>
         </div>
     )
