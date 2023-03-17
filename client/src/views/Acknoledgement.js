db64fb1b550332ce3cf4695cc61db3929cc9e9e6
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-03-16 23:00:53 -0400

    initial commit

---
+++ client/src/views/Acknoledgement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Acknoledgement = () => {
+
+    //TODO: 2 approaches I see to this. Can either hard-code in names/links or pass them in via a variable.
+        //I like the idea of the variable, it adds some reusability. An idea - maybe add an "auto carosel" to it in the future? this could be like a running list of random names in the ackoledgement variable?
+
+    return (
+    )
+}
+export default Acknoledgement;
