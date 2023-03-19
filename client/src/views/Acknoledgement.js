3b0e0fee441b9b9db67dd9c238e893093cadfbf1
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-03-18 21:09:32 -0400

    acknoledgement page

--- client/src/views/Acknoledgement.js
+++ client/src/views/Acknoledgement.js
@@ -4,9 +4,38 @@ import React from 'react';
 const Acknoledgement = () => {
 
     //TODO: 2 approaches I see to this. Can either hard-code in names/links or pass them in via a variable.
-        //I like the idea of the variable, it adds some reusability. An idea - maybe add an "auto carosel" to it in the future? this could be like a running list of random names in the ackoledgement variable?
+        //I like the idea of the variable, it adds some reusability. I can also see this getting very, very long. Perhaps there'd be a way to pull the table from readme? Or use the contributors list??
+    const contributors = [
+        {
+            name : 'Coren "Kern" Frankel',
+            LinkedIn : 'https://www.linkedin.com/in/coren-frankel/',
+            GitHub: 'https://github.com/coren-frankel'
+        },
+        {
+            name : 'Mack Smith',
+            LinkedIn : 'https://www.linkedin.com/in/macksmithlambeau/',
+            GitHub: 'https://github.com/m-smith15'
+        },
+        {
+            name : 'Vinit Hemadri',
+            GitHub : 'https://github.com/Vinitvh',
+        },
+        {
+            name : 'Diego Díaz',
+            GitHub: 'https://github.com/dialejo24'
+        }
+        ]
 
     return (
+        <div className="contributors">
+            {contributors.map((contributor, i) => (
+                <ul>
+                <li>{contributor.name}</li>
+                {contributor.LinkedIn ? <li><a href={contributor.LinkedIn}>LinkedIn</a></li> : ''}
+                <li><a href={contributor.GitHub}>GitHub</a></li>
+                </ul>
+            ))}
+        </div>
     )
 }
 export default Acknoledgement;
