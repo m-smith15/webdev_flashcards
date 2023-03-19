44d4890ea22287fe12b3bac7244e089e46628b4c
Mack Smith <106689709+m-smith15@users.noreply.github.com>
2023-03-18 20:25:36 -0400

    removing static names on footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,7 @@ const Footer = () => {
 
     return (
         <div className="footer">
-            <div className="acknowledgement">A huge thank you to the following folks for their time and energy in making this project a reality:
-                <ul>
-                    <li>Mack Smith (he/him) <a href="https://www.linkedin.com/in/macksmithlambeau/">LinkedIn </a> | <a href="https://github.com/m-smith15">GitHub</a></li>
-                    <li>Coren Frankel (he/they) <a href="https://www.linkedin.com/in/coren-frankel">LinkedIn </a> | <a href="https://github.com/coren-frankel">GitHub</a></li>
-                </ul>
+            <div className="acknowledgement">A huge thank you to the <a href="/credits">following folks</a> for their time and energy in the making of this project!
             </div>
             <div className="gettingInvolved"> Want to get involved in the project? <br />
                 Learn more about getting involved <a href="https://github.com/m-smith15/webdev_flashcards/blob/master/makingcontributions.md">here</a>!
